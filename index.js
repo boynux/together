7667e1130c6680f706019ee58f698d2c40018db4
boynux <boynux@gmail.com>
2016-07-27 22:07:23 +0200

    Store Location informationy

--- index.js
+++ index.js
@@ -9,87 +9,84 @@ var util = require('util');
 var MAX_WIDTH  = 100;
 var MAX_HEIGHT = 100;
 
-// get reference to S3 client 
-var s3 = new AWS.S3();
+AWS.config.update({
+    region: "eu-central-1",
+    // endpoint: "http://localhost:8000"
+});
 
+// get reference to S3 client 
 exports.handler = function(event, context, callback) {
   // Read options from the event.
   console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-  var srcBucket = event.Records[0].s3.bucket.name;
-  // Object key may have spaces or unicode non-ASCII characters.
-  var srcKey    =
-    decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
-  var dstBucket = srcBucket + "resized";
-  var dstKey    = "resized-" + srcKey;
 
-  // Sanity check: validate that source and destination are different buckets.
-  if (srcBucket == dstBucket) {
-    callback("Source and destination buckets are the same.");
-    return;
-  }
-
-  // Infer the image type.
-  var typeMatch = srcKey.match(/\.([^.]*)$/);
-  if (!typeMatch) {
-    callback("Could not determine the image type.");
-    return;
-  }
-  var imageType = typeMatch[1];
-  if (imageType != "jpg" && imageType != "png") {
-    callback('Unsupported image type: ${imageType}');
-    return;
-  }
-
-  // Download the image from S3, transform, and upload to a different S3 bucket.
   async.waterfall([
-      function download(next) {
-        // Download the image from S3 into a buffer.
-        s3.getObject({
-          Bucket: srcBucket,
-          Key: srcKey
-        },
-        next);
-      },
-      function transform(response, next) {
-        gm(response.Body).size(function(err, size) {
-          // Infer the scaling factor to avoid stretching the image unnaturally.
-          var scalingFactor = Math.min(
-              MAX_WIDTH / size.width,
-              MAX_HEIGHT / size.height
-              );
-          var width  = scalingFactor * size.width;
-          var height = scalingFactor * size.height;
+      function createTable(next) {
+        var dynamodb = new AWS.DynamoDB();
+        const params = {
+          TableName : "location",
+          KeySchema: [       
+          { AttributeName: "user", KeyType: "HASH"},  //Partition key
+          { AttributeName: "seen", KeyType: "RANGE" }  //Sort key
+          ],
+          AttributeDefinitions: [       
+          { AttributeName: "user", AttributeType: "S" },
+          { AttributeName: "seen", AttributeType: "S" }
+          ],
+          ProvisionedThroughput: {       
+            ReadCapacityUnits: 10, 
+            WriteCapacityUnits: 10
+          }
+        };
 
-          // Transform the image buffer in memory.
-          this.resize(width, height)
-            .toBuffer(imageType, function(err, buffer) {
-              if (err) {
-                next(err);
-              } else {
-                next(null, response.ContentType, buffer);
-              }
-            });
+        dynamodb.createTable(params, function(err, data) {
+          if (err) {
+            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+          } else {
+            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+          }
+
+          if (!err || err.code == "ResourceInUseException") {
+            next(null, true);
+          } else {
+            next(err);
+          }
+        });
+
+      },
+      function storeLocation(cont, next) {
+        var docClient = new AWS.DynamoDB.DocumentClient();
+        var params = {
+          TableName: "location",
+          Item: {
+            "user": "1",
+            "seen": new Date().toISOString(),
+            "location": {
+              "latitude": event.latitude, 
+              "longitude": event.longitude 
+            }
+          }
+        };
+
+        docClient.put(params, function(err, data) {
+          if (err) {
+            console.error("Unable to add location ", 1, ". Error JSON:", JSON.stringify(err, null, 2));
+            next(err);
+          } else {
+            console.log("PutItem succeeded:", 1);
+            next();
+          }
         });
       },
-      function upload(contentType, data, next) {
-        // Stream the transformed image to a different S3 bucket.
-        s3.putObject({
-          Bucket: dstBucket,
-          Key: dstKey,
-          Body: data,
-          ContentType: contentType
-        },
-        next);
+      function upload(next) {
+        next();
       }
   ], function (err) {
+    var msg = "Success!";
     if (err) {
-      msg = 'Unable to resize ' + srcBucket + '/' + srcKey +
-        ' and upload to ' + dstBucket + '/' + dstKey +
-        ' due to an error: ' + err;
-    } else {
-      msg = 'Successfully resized ' + srcBucket + '/' + srcKey +
-        ' and uploaded to ' + dstBucket + '/' + dstKey;
+      console.error("Error: " + err);
+      msg = "Error!";
     }
+
     context.done(err, msg);
   });
 };
