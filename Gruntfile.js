e8fdcb292a5faeeac689f8bb6ce0813b9f64e320
boynux <boynux@gmail.com>
2016-08-01 21:07:45 +0200

    Adding back Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-aws-lambda');
+
+grunt.initConfig({
+  lambda_invoke: {
+    default: {
+      options: {
+        file_name: 'index.js',
+      }
+    }
+  },
+  lambda_deploy: {
+    default: {
+      function: 'TogetherTest',
+      options: {
+        region: 'eu-central-1',
+        enableVersioning: true,
+        aliases: 'beta',
+        timeout: 30
+      }
+    }
+  },
+  lambda_package: {
+    default: {
+      options: {
+      }
+    }
+  },
+});
+
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
